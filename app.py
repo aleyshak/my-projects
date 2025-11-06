f9ac556651a4312821a12fc421b0956af29194cc
Aleysha Kaur <aleysha.k.sidhu@accenture.com>
2025-11-06 12:37:27 +0000

    Created the expense form

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     page = "<h1>Expense Tracker</h1>" # Title of the page
-    page += "<ul>"  # Start an unordered list
+    page += """
+    <form action='/add' method='POST'>
+        <input name='amount' placeholder='Amount' required>
+        <input name='description' placeholder='Description' required>
+        <button type='submit'>Add Expense</button>
+    </form>
+    """
+    page += "<ul>"
     for exp in expenses:
-        page += f"<li>${exp['amount']}: {exp['description']}</li>" # Display each expense
+        page += f"<li>${exp['amount']}: {exp['description']}</li>"
     page += "</ul>"
     return page
-
     return "Hello, Expense Tracker!"
 
 if __name__ == '__main__':
